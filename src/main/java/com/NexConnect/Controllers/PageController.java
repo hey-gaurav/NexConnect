0f26fca58121396d5d05130bdff9536125b311d1
Gaurav Singh <heygaurav07@gmail.com>
2024-11-13 13:42:45 +0530

    Understanding Fragments

--- src/main/java/com/NexConnect/Controllers/PageController.java
+++ src/main/java/com/NexConnect/Controllers/PageController.java
@@ -3,6 +3,9 @@ package com.NexConnect.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class PageController {
@@ -15,5 +18,19 @@ public class PageController {
         model.addAttribute("Age", "25");
         model.addAttribute("Gender", "Male");
         return "home";
+    }
+
+    //About Route
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("About page loading");
+        return "About";
+    }
+    
+    //Services Route
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("Services page loading");
+        return "Services";
     }
 }
